6214ba081c738774a0c346fe5210d5414ca881f5
abishejk1872 <abisheknewar15@gmail.com>
2024-08-14 15:08:43 +0530

    checking availabity of hotels

--- backend/middleware/authMiddlewares.js
+++ backend/middleware/authMiddlewares.js
@@ -1,1 +1,18 @@
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
+function authMiddleware(req, res, next) {
+    const token = req.headers.authorization
+    try {
+        const verifyId = jwt.verify(token, process.env.SECRET_KEY)
+        if (verifyId) {
+            req.userId = verifyId;
+            next()
+        }
+        return res.status(403).json({ msg: "not verified" })
+    } catch (error) {
+        console.log("eror while validating", error)
+        return res.status(403).json({ msg: "not verified" })
+    }
+}
 
+module.exports = authMiddleware
--- backend/routes/hotel.js
+++ backend/routes/hotel.js
@@ -1,22 +1,75 @@
 const express = require("express");
 const { Queries, Room, User } = require("../db");
-
+const authMiddleware = require("../middleware/authMiddlewares")
 const hotelRouter = express.Router();
 
-// hotelRouter.post("/checkavailability", async (req, res) => {
-//     const body = req.body()
-//     try {
+hotelRouter.post("/checkavailability", async (req, res) => {
+    const {fromDate,toDate, guests} = req.body;
+        
+        try {
+            const checkFromDate = parseInt(fromDate.slice(8,10))
+            const checkFromMonth = parseInt(fromDate.slice(5,8))
+            const checkToDate = parseInt(toDate.slice(8,10))
+            const checkToMonth = parseInt(toDate.slice(5,8))
+            const bookedRooms = await Room.find({})
+            console.log(bookedRooms)
+            const acRoom = bookedRooms.map((item)=>{
+                const BookedFromDate = parseInt(item.fromDate.slice(8,10))
+                const BookedFromMonth = parseInt(item.fromDate.slice(5,8))
+                const BookedToDate = parseInt(item.toDate.slice(8,10))
+                const BookedToMonth = parseInt(item.toDate.slice(5,8))
+                
+                if(BookedFromDate<= checkFromDate && BookedFromMonth <=checkFromMonth && BookedToDate>=checkToDate && BookedToMonth >=checkToMonth && item.roomType === "ac" ){
+                    return item
+                } 
+            })
+            console.log(acRoom)
+            const nonAcRoom = bookedRooms.map((item)=>{
+                const BookedFromDate = parseInt(item.fromDate.slice(8,10))
+                const BookedFromMonth = parseInt(item.fromDate.slice(5,8))
+                const BookedToDate = parseInt(item.toDate.slice(8,10))
+                const BookedToMonth = parseInt(item.toDate.slice(5,8))
+                
+                if(BookedFromDate<= checkFromDate && BookedFromMonth <=checkFromMonth && BookedToDate>=checkToDate && BookedToMonth >=checkToMonth && item.roomType === "nonac" ){
+                    return item
+                } 
+            })
+            console.log(nonAcRoom)
+            if(acRoom.length === 5 && nonAcRoom === 5){
+                return res.json({
+                    ac: false,
+                    nonac: false
+                })
+            } else if(acRoom.length === 5 && nonAcRoom.length <5){
+                return res.json({
+                    ac: false,
+                    nonac: true
+                })
+            } else if(acRoom.length < 5 && nonAcRoom.length ===5){
+                return res.json({
+                    ac: true,
+                    nonac: false
+                })
+            }else{
+                return res.json({
+                    ac: true,
+                    nonac: true
+                })
+            }
 
-//     } catch (error) {
+        
+        } catch (error) {
+            console.log("error while checking availability",error);
+            return res.status(401).json({msg: "error while checking availability"})
+        }
+    
+})
 
-//     }
-// })
-
-hotelRouter.post("/bookroom", async(req,res)=>{
+hotelRouter.post("/bookroom",authMiddleware, async(req,res)=>{
     const {fromDate,toDate,roomType,guests} = req.body;
     const userId = req.userId;
     try {
-        const bookedRooms = await Room.find({roomType: body.roomType})
+        const bookedRooms = await Room.find({roomType: roomType})
 
             bookedRooms.forEach((item)=>{
                 const BookedFromDate = parseInt(item.fromDate.slice(8,10))
@@ -32,6 +85,7 @@ hotelRouter.post("/bookroom", async(req,res)=>{
                 } 
             })
         const bookedBy = await User.findOne({_id: userId})
+        console.log(typeof(bookedBy))
         const response = await Room.create({
             fromDate: fromDate,
             toDate: toDate,
