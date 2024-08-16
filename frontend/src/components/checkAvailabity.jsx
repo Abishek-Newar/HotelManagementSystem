3b66342d2457c77fb985efd1c289e80c42bb199c
abishejk1872 <abisheknewar15@gmail.com>
2024-08-16 13:58:12 +0530

    changes in bokk rom  and ceck availability

--- frontend/src/components/checkAvailabity.jsx
+++ frontend/src/components/checkAvailabity.jsx
@@ -1,12 +1,30 @@
-import React, { useState } from 'react'
-
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
 const CheckAvailabity = () => {
   const [from,setFrom] = useState();
   const [toDate,setToDate] = useState();
   const [guest,setGuest] = useState();
-  const [roomType,setRoomType] = useState("");
-  return (
-    <div className='flex justify-around items-center py-10 flex-col md:flex-row'>
+  const [acRoom,setAcRoom] = useState(false);
+  const [nonAcRoom,setNonAcRoom] = useState(false);
+  const checkAvailableRoom = async() =>{
+    console.log(acRoom)
+    const response = await axios.post("http://localhost:3001/hotel/checkavailability",{
+      fromDate: from,
+      toDate: toDate,
+      guests: guest
+    })
+    console.log(response.data.acRoom)
+    
+    setAcRoom(response.data.ac);
+    setNonAcRoom(response.data.nonac)
+    
+  }
+  useEffect(() => {
+    console.log('acRoom updated:', acRoom);
+  }, [acRoom]);
+    return (
+    <div>
+      <div className='flex justify-around items-center py-10 flex-col md:flex-row'>
       <label htmlFor="from">
         <p className='uppercase font-bold text-2xl'>From</p>
         <input type="date" name="" id="from" className='border-2 rounded-lg h-12 w-44 p-2' onChange={(e)=>setFrom(e.target.value)} />
@@ -17,19 +35,33 @@ const CheckAvailabity = () => {
       </label>
       <label htmlFor="guest">
         <p className='uppercase font-bold text-2xl'>Guests</p>
-        <input type="number" id='guest' className='border-2 rounded-lg h-12 w-44 p-2' onChange={(e)=>setFrom(e.target.value)}/>
+        <input type="number" id='guest' className='border-2 rounded-lg h-12 w-44 p-2' onChange={(e)=>setGuest(e.target.value)}/>
       </label>
-      <div className='flex items-center gap-6'>
-      <label htmlFor="nonac" className='flex gap-4'>
-        <p className='uppercase font-bold text-2xl'>Non-AC</p>
-        <input type="radio" name='roomtype' id='nonac' className='w-4' value="AC" onChange={(e)=>setFrom(e.target.value)}/>
-      </label>
-      <label htmlFor="ac" className='flex gap-4'>
-        <p className='uppercase font-bold text-2xl'>AC</p>
-        <input type="radio" name='roomtype' id='ac' className='w-4' value="Non-AC" onChange={(e)=>setFrom(e.target.value)}/>
-      </label>
+      <button onClick={checkAvailableRoom} className='h-12 rounded-lg text-white text-lg font-bold hover:bg-blue-600 border bg-blue-400 px-6 py-2 text-center'>Check Availability</button>
+      
+    </div>
+    <div>
+        {
+          acRoom?
+          (
+            <div className='min-h-32 bg-green-400'>
+              <button>Book</button>
+            </div>
+          )
+          :
+          null
+        }
+        {
+          nonAcRoom?
+          (
+            <div className='min-h-32 bg-green-400'>
+              <button>Book</button>
+            </div>
+          )
+          :
+          null
+        }
       </div>
-      <button className='h-12 rounded-lg text-white text-lg font-bold hover:bg-blue-600 border bg-blue-400 px-6 py-2 text-center'>Check Availability</button>
     </div>
   )
 }
