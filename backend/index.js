9555307992a5be69714d51dcd1fc8bf8882c699c
abishejk1872 <abisheknewar15@gmail.com>
2024-11-27 12:33:50 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 import express from "express"
-
+import cors from "cors"
 import dbConnection from "./src/api/config/db.js"
 import env from "./src/infrastructure/env.js"
 import createRouter from "./src/infrastructure/route.js"
 
 
 const app = express()
+
+app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "PUT", "PATCH", "POST", "DELETE"],
+      credentials: true,
+      allowedHeaders: "Content-Type, Authorization",
+    })
+  );
+  
 app.use(express.json())
 dbConnection()
 app.use("/v1",createRouter())
