74c275d0c31b052374e75a66e669b2877184a81a
abishejk1872 <abisheknewar15@gmail.com>
2025-01-30 12:34:46 +0530

    changes

---
+++ frontend/src/Components/DialogBox.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const DialogBox = () => {
+  return (
+    <div>DialogBox</div>
+  )
+}
+
+export default DialogBox
+
+
+export const DialogTrigger = () =>{
+
+    return(
+        <>
+        
+        </>
+    )
+}
